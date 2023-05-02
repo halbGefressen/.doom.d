cebeb87e3f6acd593884f264a83ac5fde7585e3b
Christian Zimmerer <christian.zimmerer@protonmail.com>
2023-05-02 15:59:41 +0200

    modified some settings

--- isabelle-settings/keybindings.el
+++ isabelle-settings/keybindings.el
@@ -1,7 +1,4 @@
 ;;; isabelle-settings/keybindings.el -*- lexical-binding: t; -*-
-
-(map! :desc "Bonk" "C-ẞ" #'lsp-isar-insert-sledgehammer-and-call
-      :desc "try0" "C-ß" #'lsp-isar-insert-try0)
 ;; Accessible via SPC m
 (map! :leader :map 'isar-mode-map :localleader
       (:prefix ("t" . "Try0...")
@@ -11,17 +8,9 @@
                 :desc "Open interface"        "s" #'lsp-isar-sledgehammer-interface
                 :desc "Insert with interface" "S" #'lsp-isar-insert-sledgehammer-and-call
                 :desc "Insert sledgehammer"   "i" #'lsp-isar-insert-sledgehammer)
-        :desc "Insert auto"          "a" #'lsp-isar-insert-auto
-        :desc "Insert simp"          "b" #'lsp-isar-insert-simp
-        :desc "Insert meson"         "e" #'lsp-isar-insert-meson
-        :desc "Insert fastforce"     "f" #'lsp-isar-insert-fastforce
-        :desc "Insert algebra"       "g" #'lsp-isar-insert-algebra
-        :desc "Insert linarith"      "l" #'lsp-isar-insert-linarith
-        :desc "Insert force"         "F" #'lsp-isar-insert-force
-        :desc "Insert fast"          "o" #'lsp-isar-insert-fast
-        :desc "Insert presburger"    "p" #'lsp-isar-insert-presburger
-        :desc "Insert argo"          "r" #'lsp-isar-insert-argo
-        :desc "Insert satx"          "x" #'lsp-isar-insert-satx
-        :desc "Insert proof outline" "P" #'lsp-isar-insert-proof-outline
-        :desc "Delete windows"       "K" #'delete-other-windows ;; not quite finished
-        :desc "Open panels"          "W" #'lsp-isar-open-output-and-progress-right-isabelle)
+        :desc "Insert proof outline"    "P" #'lsp-isar-insert-proof-outline
+        :desc "Delete windows"          "K" #'delete-other-windows ;; not quite finished
+        :desc "Switch to output"        "a" (lambda () (interactive) (switch-to-buffer-other-window "*lsp-isar-output*"))
+        :desc "Switch to state"         "i" (lambda () (interactive) (switch-to-buffer-other-window "*lsp-isar-state*"))
+        :desc "Switch to progress"      "e" (lambda () (interactive) (switch-to-buffer-other-window "*lsp-isar-progress*"))
+        :desc "Open panels"             "W" #'lsp-isar-open-output-and-progress-right-isabelle)
